808237f8f965629eadadb948c439e517c301464c
poximan <poxi_man@yahoo.com>
2018-05-02 13:55:21 -0300

    avances script

--- tp2/login/mongo.bat
+++ tp2/login/mongo.bat
@@ -1,4 +1,7 @@
 C:
-REM c:\"Program Files"\MongoDB\Server\3.0\bin\mongod.exe
-C:\"Program Files"\MongoDB\Server\3.4\bin\mongod.exe
+
+cd "c:\Program Files\MongoDB\Server\3.0\bin\"
+REM C:\"Program Files"\MongoDB\Server\3.4\bin\mongod.exe
 REM C:\"Program Files"\MongoDB\Server\3.6\bin\mongod.exe
+
+mongod.exe
