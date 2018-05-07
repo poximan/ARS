708519a42bc7d7503e0e2b6fe143e18377e832d1
poximan <poxi_man@yahoo.com>
2018-05-07 20:39:46 -0300

    avances tp3

--- tp3/build.bat
+++ tp3/build.bat
@@ -1,4 +1,20 @@
 @echo off
+docker stop exec-sshd-serv
+docker rm exec-sshd-serv
+docker rmi img-sshd-serv
+
+docker stop exec-sshd-cli
+docker rm exec-sshd-cli
+docker rmi img-sshd-cli
+
+docker container prune
+docker image prune
+docker network prune
+
 docker build -t img-sshd .
 docker build -t img-sshd-serv ./servidor
 docker build -t img-sshd-cli ./cliente
+
+docker network create red-docker
+start cmd.exe -k docker run --name exec-sshd-serv --net=red-docker -ti img-sshd-serv bash
+start cmd.exe -k docker run --name exec-sshd-cli --net=red-docker -ti img-sshd-cli bash
